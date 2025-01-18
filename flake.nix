13889fc87c0f0f1192772094130550546f14a5bf
Louis Moulin <louis.moulin@outlook.fr>
2025-01-18 01:35:25 +0100

    add gitignore and working dawring module

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "my nixos config";
+  description = "NixOS and Darwin configurations";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -11,40 +11,88 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      home-manager,
-      treefmt-nix,
-    }:
+  outputs = { self, nixpkgs, home-manager, treefmt-nix, nix-darwin }:
     let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      darwinSystem = "aarch64-darwin";
+      darwinPkgs = nixpkgs.legacyPackages.${darwinSystem};
+      linuxPkgs = nixpkgs.legacyPackages.x86_64-linux;
+      treefmtEval = treefmt-nix.lib.evalModule linuxPkgs ./treefmt.nix;
     in
     {
       formatter.x86_64-linux = treefmtEval.config.build.wrapper;
-      nixosConfigurations."pc-fixe" = nixpkgs.lib.nixosSystem {
-        modules = [
-          ./pc-fixe/configuration.nix
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.llr = import ./home-manager/home.nix;
-          }
-        ];
+      
+      # NixOS configurations
+      nixosConfigurations = {
+        "pc-fixe" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./pc-fixe/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.llr = import ./home-manager/home.nix;
+            }
+          ];
+        };
+        
+        "pc-portable" = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./pc-portable/configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.llr = import ./home-manager/home.nix;
+            }
+          ];
+        };
       };
-      nixosConfigurations."pc-portable" = nixpkgs.lib.nixosSystem {
+
+      # Darwin configuration
+      darwinConfigurations."MacBook-Pro-de-Louis" = nix-darwin.lib.darwinSystem {
+        system = darwinSystem;
         modules = [
-          ./pc-portable/configuration.nix
-          home-manager.nixosModules.home-manager
           {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.llr = import ./home-manager/home.nix;
+            nixpkgs.pkgs = darwinPkgs;
+            nixpkgs.hostPlatform = darwinSystem;
+            
+            # Basic Darwin config
+            environment.systemPackages = with darwinPkgs; [
+              neovim
+	      fish
+            ];
+
+            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+            programs.fish.enable = true;
+
+            
+            system.configurationRevision = self.rev or self.dirtyRev or null;
+            system.stateVersion = 5;
+
+            users.users.llr = {
+              name = "llr";
+              home = "/Users/llr";
+	      shell = darwinPkgs.fish;
+	      userDefaultShell = darwinPkgs.fish;
+            };
+          }
+
+          # Home Manager module
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.llr = import ./home-manager/home.nix;
+            };
           }
         ];
       };
--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,15 +1,14 @@
+
 { config, pkgs, ... }:
 {
   imports = [
     ./kitty.nix
     ./fish.nix
-    ./gnome.nix
   ];
 
   home = {
     username = "llr";
-    homeDirectory = "/home/llr";
-    enableNixpkgsReleaseCheck = false;
+    homeDirectory = if pkgs.stdenv.isDarwin then "/Users/llr" else "/home/llr";
     stateVersion = "24.11";
 
     packages = with pkgs; [
@@ -17,10 +16,6 @@
       git
       vim
       neovim
-      gnumake
-      binutils
-      cmake
-      pkg-config
       python3
       python312Packages.pip
       nodejs
@@ -28,8 +23,7 @@
       sccache
       eslint_d
       nixpacks
-      gnumake
-      taplo # toml formatter
+      taplo
       typescript-language-server
 
       # System utilities
@@ -37,49 +31,29 @@
       curl
       unzip
       ripgrep
-      xclip
       bat
       eza
       zoxide
       fzf
+    ] ++ (if pkgs.stdenv.isLinux then [
+      # Linux-specific packages
+      xclip
       nmap
-      pre-commit
-      cabal-install # pre-commit deps
-      ghc # pre-commit deps
-
-      # Applications
-      jellyfin
-      jellyfin-web
-      jellyfin-ffmpeg
-      discord
-      qbittorrent
-      protonmail-desktop
-    ];
+    ] else []);
 
     sessionVariables = {
       EDITOR = "nvim";
     };
-
   };
 
   programs = {
     home-manager.enable = true;
-    nix-index = {
-      enable = true;
-    };
+    nix-index.enable = true;
     git = {
       enable = true;
       userName = "moulin-louis";
       userEmail = "louis.moulin@outlook.fr";
     };
-    firefox = {
-      enable = true;
-    };
-  };
-
-  # Your existing xresources configuration
-  xresources.properties = {
-    "Xcursor.size" = 16;
-    "Xft.dpi" = 172;
+    #firefox.enable = true;
   };
 }
